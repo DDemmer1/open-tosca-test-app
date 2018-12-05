cc54a0f2eb9fd854eeda34820c7bc2a2024d181b
DDemmer1 <Dennis.Demmer@web.de>
2018-12-05 10:31:08 +0100

    Added comments

--- src/main/java/de/demmer/dennis/opentoscatestapp/MainController.java
+++ src/main/java/de/demmer/dennis/opentoscatestapp/MainController.java
@@ -13,38 +13,47 @@ import java.util.List;
  * Ein Controller der über die GET Anfrage in der URL angesprochen wird.
  * Alle Ergebnisse werden automatisch im JSON Format zurückgegeben
  *
- * Bsp.: http://localhost:8080/id/5 gibt den User mit der ID 5 im als JSON zurück
+ * Bsp.: http://localhost:8080/id/5 gibt den User mit der ID "5" als JSON zurück
  *
  */
 @RestController
 public class MainController {
 
 
+    /**
+     * Diese Controller Methode wird aufgerufen wenn die root seite des servers aufgerufen wird (localhost:8080)
+     * Sie gibt nur einen String zurück der paar Infos zu Nutzung der REST app gibt
+     * @return
+     */
     @GetMapping(value = "/")
     public String info() {
         return "/all - Alle user anzeigen <br>/id/x - User mit der ID x anzeigen";
     }
 
+    /**
+     * Gibt alle User automatisch als JSON zurück wenn die URL ".../all" entspricht
+     * @return
+     */
     @GetMapping(value = "/all")
     public List<User> getAllUsers() {
         return OpenToscaTestAppApplication.userList;
     }
 
 
+    /**
+     * Diese Controller Methode wird aufgerufen wenn die URL .../id/{userID} entspricht
+     * Sie gibt dann einfach den User mit der entsprechenden ID zurück. Wenn kein User gefunden wurde wird nichts zurückgegeben.
+     * @param id
+     * @return
+     */
     @GetMapping(value = "/id/{userID}")
     public User getUserByID(@PathVariable("userID") int id) {
 
-        List<User> users = OpenToscaTestAppApplication.userList;
+        //Einfache Binary Search mit Java Collections nach der ID
+        int index = Collections.binarySearch(OpenToscaTestAppApplication.userList, new User(id, null, null, null), Comparator.comparingInt(User::getId));
 
-        Comparator<User> idComparator = new Comparator<User>() {
-            public int compare(User u1, User u2) {
-                return u1.getId() - u2.getId();
-            }
-        };
-        int index = Collections.binarySearch(users, new User(id, null, null, null), idComparator);
-
-        if (index >= 0) return users.get(index);
-
+        //Wenn index positiv oder null wird der entsprechende User ausgegeben
+        if (index >= 0) return OpenToscaTestAppApplication.userList.get(index);
         else return null;
     }
 }
